6a1ae8f11b4eb4dd43b4ca8121a1de9cc2eb4570
Eduardo Yanez <eduardou.yanezvidal@gmail.com>
2022-10-16 14:31:21 -0300

    Actual

--- game.js
+++ game.js
@@ -1,1 +1,49 @@
-console.log(maps)
+/** @type {HTMLCanvasElement} */
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+let canvasSize;
+let elementsSize;
+
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
+
+function setCanvasSize() {
+    if (window.innerHeight > window.innerWidth) {
+        canvasSize = window.innerWidth * 0.8;
+    } else {
+        canvasSize = window.innerHeight * 0.8;
+    }
+    
+    canvas.setAttribute('width', canvasSize);
+    canvas.setAttribute('height', canvasSize);
+
+    elementsSize = canvasSize / 10;
+
+    startGame();    
+}
+
+function startGame() {
+    //console.log({ canvasSize, elementsSize });
+
+    game.font = elementsSize + 'px Verdana';
+    game.textAlign = 'end';
+
+    for (let i = 1; i <= 10; i++) {
+        game.fillText(emojis['X'],elementsSize,elementsSize * i);
+    }
+}
+    
+
+
+
+
+
+
+    //game.fillRect(0,50,100,100);
+    //game.clearRect(0,0,50,50);
+
+    //game.font= '25px Verdana';
+    //game.fillStyle = 'purple'
+    //game.textAlign = 'center'
+    //game.fillText('FrivJavascript',25,25)
